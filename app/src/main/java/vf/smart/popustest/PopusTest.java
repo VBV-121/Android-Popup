ca6eed72bce3b7502ddabf1b50cc77d29a91560d
VBV-121 <vghadiali18@gmail.com>
2021-03-08 19:34:48 +0530

    PopUp

--- app/src/main/java/vf/smart/popustest/PopusTest.java
+++ app/src/main/java/vf/smart/popustest/PopusTest.java
@@ -4,18 +4,26 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class PopusTest extends AppCompatActivity {
 
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         showContact();
     }
 
@@ -26,23 +34,46 @@ public class PopusTest extends AppCompatActivity {
         dialog.setCancelable(true);
         dialog.show();
 
-        dialog.findViewById(R.id.btn_Contact_US).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("http://www.projectquadsolutions.com/");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+        EditText email = dialog.findViewById(R.id.email);
 
-        dialog.findViewById(R.id.btn_Tutorial).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("https://docs.google.com/document/d/1gQKFjwAT6_iHpDCUfmtIxPN6AnvWqwZB5zzTFg1gIUg/edit?usp=sharing");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+            dialog.findViewById(R.id.btn_Contact_US).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    EditText email = dialog.findViewById(R.id.email);
+                    if(email.getText().toString().trim().equals("")){
+                        Toast.makeText(getApplicationContext(), "Enter email id", Toast.LENGTH_SHORT).show();
+                    }else {
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference("message");
+
+                        myRef.setValue(email.getText().toString().trim());
+                        Uri uri = Uri.parse("http://www.projectquadsolutions.com/");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+            dialog.findViewById(R.id.btn_Tutorial).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    EditText email = dialog.findViewById(R.id.email);
+                    String e = email.getText().toString();
+                    if(TextUtils.isEmpty(e)){
+                        Toast.makeText(getApplicationContext(), "Enter email id", Toast.LENGTH_SHORT).show();
+                    }else {
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference("message");
+
+                        myRef.setValue(email.getText().toString().trim());
+                        Uri uri = Uri.parse("https://docs.google.com/document/d/1gQKFjwAT6_iHpDCUfmtIxPN6AnvWqwZB5zzTFg1gIUg/edit?usp=sharing");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+
 
     }
 
